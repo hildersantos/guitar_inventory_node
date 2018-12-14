8afc56bdf3afee09befba24d4f88183097640b5a
Hilder Santos <hilder@hildersantos.com>
2018-12-13 22:31:07 -0300

    API Routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,4 +1,5 @@
 import * as express from "express";
+import * as api from "./api";
 
 export const register = (app: express.Application) => {
   const oidc = app.locals.oidc;
@@ -6,7 +7,7 @@ export const register = (app: express.Application) => {
   // define a route handler for the default home page
   app.get("/", (req: any, res) => {
     const user = req.userContext ? req.userContext.userinfo : null;
-    res.render("index", {isAuthenticated: req.isAuthenticated(), user});
+    res.render("index", { isAuthenticated: req.isAuthenticated(), user });
   });
 
   // define a secure route handler for the login page that redirects to /guitars
@@ -23,7 +24,8 @@ export const register = (app: express.Application) => {
   // define a secure route handler for the guitars page
   app.get("/guitars", oidc.ensureAuthenticated(), (req: any, res) => {
     const user = req.userContext ? req.userContext.userinfo : null;
-    res.render("guitars", {isAuthenticated: req.isAuthenticated(), user});
+    res.render("guitars", { isAuthenticated: req.isAuthenticated(), user });
   });
 
+  api.register(app);
 };
